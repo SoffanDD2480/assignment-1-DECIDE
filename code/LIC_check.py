8414faa432520a2b2d14031e44a032da89add064
Elias <elias.frode@hotmail.com>
2025-01-23 16:23:02 +0100

    fix edge case for data_points in LIC_0_check

--- code/LIC_check.py
+++ code/LIC_check.py
@@ -5,6 +5,8 @@ def lic_0_check(data_points, length1):
     """Function for checking requirement LIC 0. Returns True
     if there exists 2 consecutive points with a distance
     larger than length1, False otherwie"""
+    if len(data_points) < 2:
+        return False
     for index in range(len(data_points)-1):
         (x_1, y_1) = data_points[index]
         (x_2, y_2) = data_points[index + 1]
