99c5c1c21a9e1f574410798debb8c022d9598cef
Elias <elias.frode@hotmail.com>
2025-01-27 16:26:30 +0100

    fix special case LIC_1_check function

--- code/LIC_check.py
+++ code/LIC_check.py
@@ -1,5 +1,5 @@
 from math import sqrt
-import sympy
+from sympy import Eq, solve, symbols
 
 
 def lic_0_check(data_points, length1):
@@ -26,31 +26,33 @@ def lic_1_check(data_points, radius1):
         return False
     for index in range(len(data_points) - 2):
         [(x_1, y_1), (x_2, y_2), (x_3, y_3)] = (data_points[index], data_points[index + 1], data_points[index + 2])
-        distAB = sqrt((x_2 - x_1)**2 + (y_2 - y_1)**2) > 2*radius1
-        distAC = sqrt((x_3 - x_1)**2 + (y_3 - y_1)**2) > 2*radius1
-        distBC = sqrt((x_3 - x_2)**2 + (y_3 - y_2)**2) > 2*radius1
-        if distAB or distAC or distBC:
+        dist1 = sqrt((x_2 - x_1)**2 + (y_2 - y_1)**2) > 2*radius1
+        dist2 = sqrt((x_3 - x_1)**2 + (y_3 - y_1)**2) > 2*radius1
+        dist3 = sqrt((x_3 - x_2)**2 + (y_3 - y_2)**2) > 2*radius1
+        if dist1 or dist2 or dist3:
             return True
         midpoints = [((x_1 + x_2)/2, (y_1 + y_2)/2),
                      ((x_1 + x_3)/2, (y_1 + y_3)/2),
                      ((x_2 + x_3)/2, (y_2 + y_3)/2)]
+        if midpoints[0] in midpoints[1:] or midpoints[1] == midpoints[2]:  # Check if any points coincide
+            continue
         k_values = [(y_2 - y_1)/(x_2 - x_1) if x_1 != x_2 else None,
                   (y_3 - y_1)/(x_3 - x_1) if x_1 != x_3 else None,
                   (y_3 - y_2)/(x_3 - x_2) if x_2 != x_3 else None]
-        if k_values[0] == k_values[1] == k_values[2]:
+        if k_values[0] == k_values[1] == k_values[2]:  # Colinearity check ==> no circumcenter
             continue
         equations = []
-        x, y = sympy.symbols('x y')
+        x, y = symbols('x y')
         for slope, coord in zip(k_values, midpoints):
             if slope == 0:
                 pass
             elif slope is None:
-                equation = sympy.Eq(y - coord[1], 0)
+                equation = Eq(y - coord[1], 0)
                 equations.append(equation)
             else:
-                equation = sympy.Eq(y - coord[1], -(1/slope)*(x - coord[0]))
+                equation = Eq(y - coord[1], -(1/slope)*(x - coord[0]))
                 equations.append(equation)
-        circumcenter = sympy.solve((equations[0], equations[1]), (x, y))
+        circumcenter = solve((equations[0], equations[1]), (x, y))
         if sqrt((circumcenter[x] - x_1)**2 + (circumcenter[y] - y_1)**2) > radius1:
             return True
     return False
