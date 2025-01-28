7d312f8902bfda169fa65f62926081b14679a873
Elias <elias.frode@hotmail.com>
2025-01-28 15:42:44 +0100

    feat LIC 13 check implemented
    function calculate_circumcenter implemented and used for LIC 1 and LIC 13.

--- code/LIC_check.py
+++ code/LIC_check.py
@@ -19,6 +19,37 @@ def calculate_triangle_area(p1, p2, p3) -> float:
     return area
 
 
+def calculate_circumcenter(p1, p2, p3):
+    """Calculate circumcenter of triangle.
+    Returns coordinates of circumcenter,
+    otherwise None if input is not a triangle"""
+    (x_1, y_1), (x_2, y_2), (x_3, y_3) = p1, p2, p3
+    midpoints = [((x_1 + x_2) / 2, (y_1 + y_2) / 2),
+                 ((x_1 + x_3) / 2, (y_1 + y_3) / 2),
+                 ((x_2 + x_3) / 2, (y_2 + y_3) / 2)]
+    if midpoints[0] in midpoints[1:] or midpoints[1] == midpoints[2]:  # Check if any points coincide
+        return None
+    k_values = [(y_2 - y_1) / (x_2 - x_1) if x_1 != x_2 else None,
+                (y_3 - y_1) / (x_3 - x_1) if x_1 != x_3 else None,
+                (y_3 - y_2) / (x_3 - x_2) if x_2 != x_3 else None]
+    if k_values[0] == k_values[1] == k_values[2]:  # Colinearity check ==> no circumcenter
+        return None
+    equations = []
+    x, y = symbols('x y')
+    for slope, coord in zip(k_values, midpoints):
+        if slope == 0:
+            pass
+        elif slope is None:
+            equation = Eq(y - coord[1], 0)
+            equations.append(equation)
+        else:
+            equation = Eq(y - coord[1], -(1 / slope) * (x - coord[0]))
+            equations.append(equation)
+    circumcenter = solve((equations[0], equations[1]), (x, y))
+    to_tuple = (circumcenter[x], circumcenter[y])
+    return to_tuple
+
+
 def lic_0_check(data_points, length1):
     """Function for checking requirement LIC 0. Returns True
     if there exists 2 consecutive points with a distance
@@ -42,36 +73,18 @@ def lic_1_check(data_points, radius1):
     if len(data_points) < 3:
         return False
     for index in range(len(data_points) - 2):
-        (x_1, y_1), (x_2, y_2), (x_3, y_3) = p1, p2, p3 = \
-            (data_points[index], data_points[index + 1], data_points[index + 2])
+        p1, p2, p3 = (data_points[index],
+                      data_points[index + 1],
+                      data_points[index + 2])
         dist1 = calculate_distance(p1, p2) > 2 * radius1
         dist2 = calculate_distance(p1, p3) > 2 * radius1
         dist3 = calculate_distance(p2, p3) > 2 * radius1
         if dist1 or dist2 or dist3:
             return True
-        midpoints = [((x_1 + x_2)/2, (y_1 + y_2)/2),
-                     ((x_1 + x_3)/2, (y_1 + y_3)/2),
-                     ((x_2 + x_3)/2, (y_2 + y_3)/2)]
-        if midpoints[0] in midpoints[1:] or midpoints[1] == midpoints[2]:  # Check if any points coincide
+        circumcenter = calculate_circumcenter(p1, p2, p3)
+        if circumcenter is None:
             continue
-        k_values = [(y_2 - y_1)/(x_2 - x_1) if x_1 != x_2 else None,
-                  (y_3 - y_1)/(x_3 - x_1) if x_1 != x_3 else None,
-                  (y_3 - y_2)/(x_3 - x_2) if x_2 != x_3 else None]
-        if k_values[0] == k_values[1] == k_values[2]:  # Colinearity check ==> no circumcenter
-            continue
-        equations = []
-        x, y = symbols('x y')
-        for slope, coord in zip(k_values, midpoints):
-            if slope == 0:
-                pass
-            elif slope is None:
-                equation = Eq(y - coord[1], 0)
-                equations.append(equation)
-            else:
-                equation = Eq(y - coord[1], -(1/slope)*(x - coord[0]))
-                equations.append(equation)
-        circumcenter = solve((equations[0], equations[1]), (x, y))
-        if calculate_distance(circumcenter, (x_1, y_1)) > radius1:
+        if calculate_distance(circumcenter, p1) > radius1:
             return True
     return False
 
@@ -339,6 +352,29 @@ def lic_12_check(data_points, numpoints, k_pts, length1, length2):
             return True
 
     # No match found
+    return False
+
+
+def lic_13_check(data_points, radius1, a_pts, b_pts):
+    """Function for LIC 13. Similar to
+    LIC 1 implementation"""
+    if len(data_points) < a_pts + b_pts + 3 or len(data_points) < 5:
+        return False
+    for index in range(len(data_points) - a_pts - b_pts - 2):
+        p1, p2, p3 = (data_points[index],
+                      data_points[index + a_pts + 1],
+                      data_points[index + a_pts + b_pts + 2])
+        dist1 = calculate_distance(p1, p2) > 2*radius1
+        dist2 = calculate_distance(p1, p3) > 2*radius1
+        dist3 = calculate_distance(p2, p3) > 2*radius1
+        if dist1 or dist2 or dist3:
+            return True
+        circumcenter = calculate_circumcenter(p1, p2, p3)
+        print(circumcenter)
+        if circumcenter is None:
+            continue
+        if calculate_distance(circumcenter, p1) > radius1:
+            return True
     return False
 
 
